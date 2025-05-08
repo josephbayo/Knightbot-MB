ce7319c802a964e288acc708f97e2eac7a15deb1
josephbayo <fixajo2004@gmail.com>
2025-05-08 20:28:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'INSANITY', // Your name
-  ownerNumber: '2349136791372', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349058741028', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
